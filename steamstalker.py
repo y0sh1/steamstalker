0d15e64b3c07c08cdc3f4159ba6b107d1b29a66f
y0sh1 <joshua@theinternets.nl>
2019-08-15 12:30:42 +0200

    updated with a new dockerfile

--- steamstalker.py
+++ steamstalker.py
@@ -1,5 +1,5 @@
 import steam
-from sqlalchemy import create_engine, Column, Integer, DateTime
+from sqlalchemy import create_engine, Column, Integer, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import argparse
@@ -36,25 +36,36 @@ class Activity(Base):
     gameid = Column(Integer, nullable=True)
     event_datetime = Column(DateTime, nullable=False)
 
+
+class SteamGameName(Base):
+    __tablename__ = 'steamgamename'
+
+    id = Column(Integer, primary_key=True)
+    gameid = Column(Integer, nullable=False)
+    gamename = Column(String, nullable=False)
+
+
 if args.install:
     Base.metadata.create_all()
 
+try:
+    steam.api.key.set(config['steam']['apikey'])
 
-steam.api.key.set(config['steam']['apikey'])
+    profile_ids = []
 
-profile_ids = []
+    for url in config['steam']['profiles']:
+        profile_ids.append(steam.user.vanity_url(url))
 
-for url in config['steam']['profiles']:
-    profile_ids.append(steam.user.vanity_url(url))
+    activities = []
 
-activities = []
+    for profile_id in profile_ids:
+        activities.append(Activity(steamid=profile_id.id64,
+                                   gameid=steam.user.profile(profile_id).current_game[0],
+                                   event_datetime=datetime.now()))
 
-for profile_id in profile_ids:
-    activities.append(Activity(steamid=profile_id.id64,
-                               gameid=steam.user.profile(profile_id).current_game[0],
-                               event_datetime=datetime.now()))
-
-session.add_all(activities)
-session.commit()
-session.flush()
-
+    session.add_all(activities)
+    session.commit()
+    session.flush()
+except:
+    import traceback
+    print(traceback.format_exc())
